4418a9a2df1284dfcee0cd4832d256493568a7f9
Lixd1985 <lxd78858139@gmail.com>
2017-04-03 14:06:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if(args[0].indexOf('@') > -1)
 
 
 appid   =   args[2] || 'all',
-images  =   ["lixd1985/ss"];
+images  =   ["lixd1985/ss","lowid/ss-with-net-speeder","smounives/shadowsocksr-docker","malaohu/ssr-with-net-speeder"];
 
 
 app.get('/', function(req, res) {
